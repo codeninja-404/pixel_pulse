6a771b90c81bbbf5ca275135f23a6f8474d0e1e8
codeninja-404 <saidularefin8@gmail.com>
2024-04-15 23:01:11 +0600

    Created pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,24 @@
+
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AboutPage from "./Pages/AboutPage/AboutPage";
+import HomePage from "./Pages/HomePage/HomePage";
+import SignInPage from "./Pages/SignInPage/SignInPage";
+import SignUpPage from "./Pages/SignUpPage/SignUpPage";
+import Dashboard from "./Pages/Dashboard/Dashboard";
+import ProjectsPage from "./Pages/ProjectsPage/ProjectsPage";
 const App = () => {
   return (
-    <div>
-      hello
-    </div>
+    <BrowserRouter>
+     <Routes>
+     <Route path="/" element={<HomePage></HomePage>}></Route>
+      <Route path="/about" element={<AboutPage></AboutPage>}></Route>
+      <Route path="/sign-in" element={<SignInPage />}></Route>
+      <Route path="/sign-up" element={<SignUpPage />}></Route>
+      <Route path="/dashboard" element={<Dashboard />}></Route>
+      <Route path="/projects" element={<ProjectsPage />}></Route>
+     </Routes>
+    </BrowserRouter>
   );
 };
 
