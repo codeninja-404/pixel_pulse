c30d10bafbf92a8c011e1243b43f8ba3fdea1838
codeninja-404 <saidularefin8@gmail.com>
2024-04-16 21:45:45 +0600

    installed flowvite and react icons and ceated nav component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,25 @@
 
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AboutPage from "./Pages/AboutPage/AboutPage";
-import HomePage from "./Pages/HomePage/HomePage";
-import SignInPage from "./Pages/SignInPage/SignInPage";
-import SignUpPage from "./Pages/SignUpPage/SignUpPage";
-import Dashboard from "./Pages/Dashboard/Dashboard";
-import ProjectsPage from "./Pages/ProjectsPage/ProjectsPage";
+import AboutPage from "./pages/AboutPage/AboutPage";
+import HomePage from "./pages/HomePage/HomePage";
+import SignInPage from "./pages/SignInPage/SignInPage";
+import SignUpPage from "./pages/SignUpPage/SignUpPage";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import ProjectsPage from "./pages/ProjectsPage/ProjectsPage";
+import Header from "./components/Header/Header";
 const App = () => {
   return (
     <BrowserRouter>
-     <Routes>
-     <Route path="/" element={<HomePage></HomePage>}></Route>
-      <Route path="/about" element={<AboutPage></AboutPage>}></Route>
-      <Route path="/sign-in" element={<SignInPage />}></Route>
-      <Route path="/sign-up" element={<SignUpPage />}></Route>
-      <Route path="/dashboard" element={<Dashboard />}></Route>
-      <Route path="/projects" element={<ProjectsPage />}></Route>
-     </Routes>
+    <Header/>
+      <Routes>
+        <Route path="/" element={<HomePage></HomePage>}></Route>
+        <Route path="/about" element={<AboutPage></AboutPage>}></Route>
+        <Route path="/sign-in" element={<SignInPage />}></Route>
+        <Route path="/sign-up" element={<SignUpPage />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/projects" element={<ProjectsPage />}></Route>
+      </Routes>
     </BrowserRouter>
   );
 };
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,28 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+    
+//     'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [
+//     require('flowbite/plugin'),
+    
+//   ],
+// };
+const flowbite = require("flowbite-react/tailwind");
+
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    flowbite.content(),
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+  plugins: [
+    // ...
+    flowbite.plugin(),
+  ],
+};
