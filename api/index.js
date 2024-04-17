68e17304337238352a37894e5283e56ce4941f7b
codeninja-404 <saidularefin8@gmail.com>
2024-04-17 18:32:40 +0600

    connected tthe server to the mongodb database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODBURI)
+  .then(() => {
+    console.log("Db connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
-app.listen(5000,()=>{
-    console.log('Server running on port 5000')
-})
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
