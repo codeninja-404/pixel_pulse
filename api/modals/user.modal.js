6a4e142b07eab439b9ecaee666120bf8be087fa9
Saidul Arefin <saidularefin8@gmail.com>
2024-06-04 21:07:01 +0600

    added admin functionality to the user

--- api/modals/user.modal.js
+++ api/modals/user.modal.js
@@ -20,8 +20,13 @@ const userSchema = new mongoose.Schema(
       default:
         "https://cdn.pixabay.com/photo/2015/03/04/22/35/avatar-659651_960_720.png",
     },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
   },
-  { timestamps: true }
+
+  { timestamps: true },
 );
 
 const User = mongoose.model("User", userSchema);
