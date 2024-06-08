0a33c102c0f78508171b82235f8897199646b62b
Saidul Arefin <saidularefin8@gmail.com>
2024-06-08 23:59:24 +0600

    show posts inside dashboard ui is complete

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,7 @@
 // /** @type {import('tailwindcss').Config} */
 // export default {
 //   content: [
-    
+
 //     'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
 //   ],
 //   theme: {
@@ -9,20 +9,16 @@
 //   },
 //   plugins: [
 //     require('flowbite/plugin'),
-    
+
 //   ],
 // };
 const flowbite = require("flowbite-react/tailwind");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-    flowbite.content(),
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
   plugins: [
-    // ...
+    require("tailwind-scrollbar"), // Add this line
     flowbite.plugin(),
   ],
 };
