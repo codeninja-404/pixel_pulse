68d61e42da3b0ac1e9affdb1a1b86c0808d4b42a
Saidul Arefin <saidularefin8@gmail.com>
2024-07-02 19:39:30 +0600

    customizing the navigationbar

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -7,11 +7,33 @@ import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { toggleTheme } from "../../redux/theme/themeSlice";
 import { signoutSuccess } from "../../redux/user/userSlice";
+import { useState } from "react";
 const Header = () => {
   const path = useLocation().pathname;
   const dispatch = useDispatch();
   const { currentUser } = useSelector((state) => state.user);
   const { theme } = useSelector((state) => state.theme);
+  const [toggleNav, setToggleNav] = useState(false);
+
+  const navlinks = (
+    // <div className="">
+    <ul
+      className={`absolute mt-4 flex flex-col md:mt-0 md:flex-row md:text-sm md:font-medium  md:block md:w-auto  transform transition-transform duration-300 rounded-b-lg shadow-md ${
+        toggleNav ? "translate-y-0 top-[58px]" : " -translate-y-full"
+      }  left-0 right-0  py-2 text-center bg-[#1f2937]`}
+    >
+      <li className="p-2  border-2" active={path === "/"} as={"div"}>
+        <Link to="/">Home</Link>
+      </li>
+      <li className="p-2 border-2 " active={path === "/about"} as={"div"}>
+        <Link to="/about">About</Link>
+      </li>
+      <li className="p-2 border-2 " active={path === "/projects"} as={"div"}>
+        <Link to="/projects">Projects</Link>
+      </li>
+    </ul>
+    // </div>
+  );
 
   const handleSignout = async () => {
     try {
@@ -27,7 +49,7 @@ const Header = () => {
     }
   };
   return (
-    <Navbar className="border-b-2 fixed w-screen z-50">
+    <Navbar className="border-b-2 fixed w-screen z-50 navbar flex flex-col">
       <Link to="/">
         <img className="w-20 object-contain" src={logo} alt="" />
       </Link>
@@ -93,19 +115,13 @@ const Header = () => {
             </Link>
           )}
         </Link>
-        <Navbar.Toggle />
+        <Button
+          onClick={() => setToggleNav(!toggleNav)}
+          className=""
+          color="gray"
+        />
       </div>
-      <Navbar.Collapse>
-        <Navbar.Link active={path === "/"} as={"div"}>
-          <Link to="/">Home</Link>
-        </Navbar.Link>
-        <Navbar.Link active={path === "/about"} as={"div"}>
-          <Link to="/about">About</Link>
-        </Navbar.Link>
-        <Navbar.Link active={path === "/projects"} as={"div"}>
-          <Link to="/projects">Projects</Link>
-        </Navbar.Link>
-      </Navbar.Collapse>
+      {navlinks}
     </Navbar>
   );
 };
